782f8c71053aa333c82f001208d3edc3435aff24
Gonzalo Melosevich <gonzalo.melosevich@spark.net>
2026-01-18 06:11:09 +0100

    feat: update finops_budget module name for production environment

--- states/prod/base/main.tf
+++ states/prod/base/main.tf
@@ -1,7 +1,7 @@
 module "finops_budget" {
   source = "../../../modules/finops/budget-guardrails"
 
-  name         = "dev-finops-monthly-budget"
+  name         = "prod-finops-monthly-budget"
   limit_amount = 50
   alert_email  = var.alert_email
 }
